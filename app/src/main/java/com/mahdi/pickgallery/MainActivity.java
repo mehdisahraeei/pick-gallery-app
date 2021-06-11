5a32cfc6c3ea852f19db0c37c7f6a388e092e2d6
mahdi <mahdi_sahrai@yahoo.com>
2021-06-11 12:16:23 +0430

    Saving in file manager, path from file manager

--- app/src/main/java/com/mahdi/pickgallery/MainActivity.java
+++ app/src/main/java/com/mahdi/pickgallery/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mahdi.pickgallery;
 
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -12,24 +13,31 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button1, button2;
     private ImageView imageView;
-    private int Request_File = 222;
+    private TextView textView;
+    private int Request_File = 1;
     private int Request_Storage = 111;
     private Uri uri;
     private String stringPath;
-    private Intent Data;
+    private OutputStream outputStream;
 
 
     @Override
@@ -37,11 +45,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.btn1);
+        button1 = findViewById(R.id.btn1);
+        button2 = findViewById(R.id.btn2);
         imageView = findViewById(R.id.image1);
+        textView = findViewById(R.id.tone);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PathActivity.class));
+            }
+        });
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -53,48 +71,60 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
-
-
-
-
 
 
     private void selectImage() {
-        startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), Request_File);
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, Request_File);
     }
-
-
-
-
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == Request_File && resultCode == RESULT_OK) {
-            if (data != null) {
-                uri = data.getData();
-                Data = data;
 
-                getStringPath(uri);
+        if (requestCode == Request_File && resultCode == RESULT_OK && data != null && data.getData() != null) {
 
-                try {
-                    InputStream inputStream = getContentResolver().openInputStream(uri);
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                    imageView.setImageBitmap(bitmap);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
 
+            File filepath = Environment.getExternalStorageDirectory();
+            File dir = new File(filepath.getAbsolutePath() + "/lib/");
+            dir.mkdirs();
+            File file = new File(dir, "one.JPEG");
+
+            uri = data.getData();
+            getStringPath(uri);
+
+
+            InputStream inputStream = null;
+            try {
+                inputStream = getContentResolver().openInputStream(uri);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
             }
+
+
+
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            imageView.setImageBitmap(bitmap);
+
+            outputStream = null;
+
+
+            try {
+                outputStream = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                Toast.makeText(MainActivity.this, "save", Toast.LENGTH_SHORT).show();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
         }
     }
-
-
 
 
     public String getStringPath(Uri uriN) {
@@ -110,7 +140,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return stringPath;
     }
-
 
 
 }
---
+++ app/src/main/java/com/mahdi/pickgallery/PathActivity.java
@@ -0,0 +1,59 @@
+package com.mahdi.pickgallery;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PathActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_path);
+
+        button = findViewById(R.id.b1);
+        textView = findViewById(R.id.t1);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                startActivityForResult(intent, 10);
+            }
+        });
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK) {
+                    String path = data.getData().getPath();
+                    textView.setText(path);
+                }
+                break;
+        }
+
+    }
+
+
+
+    
+}
+
